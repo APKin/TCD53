6c175a93065641c04f075082d58a3dcd78cbf63f
qimiaoyi <617530216@qq.com>
2025-06-14 14:24:36 +0800

    DBHelper调整

--- TowerController/DBHelper.h
+++ TowerController/DBHelper.h
@@ -13,8 +13,6 @@ class DBHelper
 	QSqlDatabase db;
 public:
 	DBHelper() {
-		db = QSqlDatabase::addDatabase("QSQLITE", "tower");
-		db.setDatabaseName("sqlite3/tower.db");
 	}
 	bool open() {
 		//如果db不为空并且已经打开，则直接返回true
@@ -24,7 +22,16 @@ public:
 		db.open();
 
 	}
-	QSqlDatabase getDatabase() { return db; }
+	QSqlDatabase getDatabase() { 
+        if (!db.isValid()) {
+			db = QSqlDatabase::addDatabase("QSQLITE", "tower");
+			db.setDatabaseName("sqlite3/tower.db");
+		}
+		if  (!db.isOpen()) {
+			db.open();
+		}
+		return db; 
+	}
 	//插入数据，传入参数是表名，多个字段名，多个字段值,返回插入成功与否
 	bool insertData(const QString& tableName, QVariantMap& fvMap)
 	{
